5987d2ee1f650f9cace1418791c5c3c14eb32d54
Salomon <salopere1107@gmail.com>
2023-10-26 17:24:42 -0400

    first commit

--- CRUDapp/CRUDapp/src/test/java/com/csc340/CRUDapp/CruDappApplicationTests.java
+++ CRUDapp/CRUDapp/src/test/java/com/csc340/CRUDapp/CruDappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.csc340.CRUDapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CruDappApplicationTests {
 
 	@Test
 	void contextLoads() {
